7d42a177543c4fb1673f14e966d5c926af3e4716
Zhenia Egorov <zheniaegorov1999@gmail.com>
2025-05-23 03:40:16 +0300

    first

---
+++ Egorov_Repository_LR4_0/Egorov_project_LR4.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <functional>
+
+
+int side1, side2;
+
+// Проверка корректности вводимых данных
+bool UserInput(std::string input) {
+    // Если строка пустая - ввод некорректен
+    if (input.empty()) return false;
+    
+    // Попытаться преобразовать введенное значение в int
+    try {
+        int number = stoi(input);
+        if (number < 0) return false; // Отрицательные числа недопустимы
+    }
+    catch (...) { // Если возникла ошибка (например, нечисловой ввод)
+        return false;
+    }
+    return true; // Ввод корректен
+}
+
+// Функция ввода данных
+void enterNumber(int& varLink, std::string label) {
+    std::string str_input;
+    std::cout << label << " = ";
+    std::getline(std::cin, str_input); // Ввод значения в текстовом виде
+    
+    // Пока ввод некорректен, повторять запрос
+    while (!UserInput(str_input)) {
+        std::cout << label << " = ";
+        std::getline(std::cin, str_input);
+    }
+    
+    // Присвоить переменной varLink преобразованное значение
+    varLink = stoi(str_input);
+}
+
+void enterSide1()
+{
+    //разрабатывается Developer1 - ветка branch_fun_1
+}
+
+void enterSide2()
+{
+    //разрабатывается Developer2 - ветка branch_fun_2
+}
+
+void calcArea()
+{
+    //разрабатывается Developer3 - ветка branch_fun_3
+}
+
+void calcPerim()
+{
+    //разрабатывается Developer4 - ветка branch_fun_4
+}
+
+struct MenuItem {
+    std::string title;
+    std::function<void()> action; 
+};
+
+int main() {
+    std::map<int, MenuItem> menu = {
+        {1, {"Ввод 1-й стороны прямоугольника", enterSide1}},
+        {2, {"Ввод 2-й стороны прямоугольника", enterSide2}},
+        {3, {"Calculate of Area", calcArea}},
+        {4, {"Calculate of Perimetr", calcPerim}}
+    };
+
+    int choice = 0;
+
+    while (true) {
+        std::cout << "Меню:" << "\n";
+        for (const auto& item : menu) {
+            std::cout << "Task " << item.first << ". " << item.second.title << "\n";
+        }
+        std::cout << "0. Выход" << "\n";
+        enterNumber(choice, "Input number of menu: ");
+        if (choice == 0) {
+            std::cout << "0 2025 FirstName LastName" << "\n";
+            break;
+        }
+        std::cout << '\n';
+        if (menu.find(choice) != menu.end()) {
+            menu[choice].action();
+        } else {
+            std::cout << "Некорректный ввод.";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+}
